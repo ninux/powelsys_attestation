7f3051f184abe3ac0cde525b5d84689897f0f7e3
Nino Ninux <nino.ninux@gmail.com>
2017-11-01 09:56:45 +0100

    added function description and help text

--- sim/attestation.m
+++ sim/attestation.m
@@ -1,31 +1,31 @@
-% cleanup
+% cleanup ----------------------------------------------------------------------
 clear all;
 close all;
 
-% load packages
+% load packages ----------------------------------------------------------------
 pkg load control;
 pkg load signal;
 
-% setup graphics
+% setup graphics ---------------------------------------------------------------
 %graphics_toolkit gnuplot
 graphics_toolkit qt
 
-% definitions
+% definitions ------------------------------------------------------------------
 vo  = 200;    % [V]   output voltage
 L   = 200E-3; % [H]   inductance
 C   = 5E-3;   % [F]   output capacitance
 R   = 50;     % [Ohm] load resistance
 fs  = 100E3;  % [Hz]  switching frequency
 
-% 1A
+% 1A ---------------------------------------------------------------------------
 vi1 = 50;
 vi2 = 100;
 vi3 = 150;
 vi  = [vi1, vi2, vi3];
 
-d1 = boostduty(vi1,vo);
-d2 = boostduty(vi2,vo);
-d3 = boostduty(vi3,vo);
+d1 = boostduty(vo, vi1);
+d2 = boostduty(vo, vi2);
+d3 = boostduty(vo, vi3);
 
 fprintf("results for 1-A:");
 fprintf("\n\tD(Vi = %dV) \t= %.2f", vi1, d1);
@@ -33,13 +33,13 @@ fprintf("\n\tD(Vi = %dV) \t= %.2f", vi2, d2);
 fprintf("\n\tD(Vi = %dV) \t= %.2f", vi2, d3);
 fprintf("\n---\n");
 
-% 1B
-s = tf('s');
-h1 = tf( (vo/d1) * (1 - s*L/R/(1-d1)^2) / (1 + s*L/R/(1-d1)^2 + s^2*L*C/(1-d1)^2) );
-h2 = tf( (vo/d2) * (1 - s*L/R/(1-d2)^2) / (1 + s*L/R/(1-d2)^2 + s^2*L*C/(1-d2)^2) );
-h3 = tf( (vo/d3) * (1 - s*L/R/(1-d3)^2) / (1 + s*L/R/(1-d3)^2 + s^2*L*C/(1-d3)^2) );
+% 1B ---------------------------------------------------------------------------
+s  = tf('s');
+h1 = tf((vo/d1)*(1 - s*L/R/(1-d1)^2) / (1 + s*L/R/(1-d1)^2 + s^2*L*C/(1-d1)^2));
+h2 = tf((vo/d2)*(1 - s*L/R/(1-d2)^2) / (1 + s*L/R/(1-d2)^2 + s^2*L*C/(1-d2)^2));
+h3 = tf((vo/d3)*(1 - s*L/R/(1-d3)^2) / (1 + s*L/R/(1-d3)^2 + s^2*L*C/(1-d3)^2));
 
-H = [h1;h2;h3];
+
 
 % 1C
 fc = 1/10 * fs/2;
--- sim/boostduty.m
+++ sim/boostduty.m
@@ -1,3 +1,29 @@
-function d = boostduty(vi, vo)
-  d = (-vi + vo) / (vo) ;
+function d = boostduty(vo, vi)
+% BOOSTDUTY Calculate the duty cycle of a boost converter.
+%
+%   d = BOOSTDUTY(vo,vi)
+%     Description:
+%       Using absolute input and output voltages to calculate the duty cycle.
+%     Output:
+%       d   duty cycle as unitless factor [0..1]
+%     Input:
+%       vi  input voltage in [V]
+%       vo  output voltage [V]
+%
+%   d = BOOSTDUTY(vo)     using relative output voltage (out / in)
+%     Description:
+%       Using relative output voltage to calculate the duty cycle.
+%     Output:
+%       d   duty cycle as unitless factor [0..1]
+%     Input:
+%       vo  relative output voltage (output / input) as unitless factor [1..n]
+
+  switch nargin
+    case 2
+      d = (-vi + vo) / (vo) ;
+    case 1
+      d = (-1 + vo) / (vo);
+    otherwise
+      d = -1;
+  endswitch
 endfunction
